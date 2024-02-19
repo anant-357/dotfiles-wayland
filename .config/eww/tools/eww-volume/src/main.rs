ae25d45c7a607b8cbecd2cb936fa893b8dbe1788
anant-357 <anantsh.88@gmail.com>
2024-02-19 12:43:21 +0530

    added bash script for volume and eww-volume widget

--- .config/eww/tools/eww-volume/src/main.rs
+++ .config/eww/tools/eww-volume/src/main.rs
@@ -1,5 +1,8 @@
-use pul
-    
+use pipewire::{main_loop:: MainLoop, context:: Context};    
 fn main() {
-
+    let mainloop = MainLoop::new(None).expect("mainloop");
+    let context = Context::new(&mainloop).expect("context");
+    let core = context.connect(None).expect("core");
+    let registry = core.get_registry().expect("registery");
+    println!("{:#?}", registry);
 }
