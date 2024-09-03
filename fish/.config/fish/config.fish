c3589994a41645e9b4f964e472bf7d32cd99dd36
Anant Sharma <anantsh.88@gmail.com>
2024-09-03 16:12:35 +0530

    updates

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,10 @@
 set -Ux PYENV_ROOT $HOME/.pyenv
 set -U fish_user_paths $PYENV_ROOT/bin $fish_user_paths
 
+alias pari "paru"
+alias cat "bat"
+alias ls "eza"
+
 if status is-interactive
 
     # Commands to run in interactive sessions can go here
