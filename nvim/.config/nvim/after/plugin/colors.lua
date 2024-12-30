89d884908594177e23e8cbc8f5a10e432a454a00
Anant Sharma <anantsh.88@gmail.com>
2024-12-30 13:16:00 +0530

    update: nvim

--- nvim/.config/nvim/after/plugin/colors.lua
+++ nvim/.config/nvim/after/plugin/colors.lua
@@ -1,5 +1,11 @@
-require'nvim-web-devicons'.setup {}
+require 'nvim-web-devicons'.setup {}
 
 vim.cmd.colorscheme('gruvbox')
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" } )
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" } )
+-- vim.cmd.colorscheme('catppuccin')
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+
+-- require("catppuccin").setup({
+--     flavour = "frappe"
+-- })
